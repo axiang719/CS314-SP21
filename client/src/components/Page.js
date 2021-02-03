82d4054f71c49fac19584d2d6dae7b90d264e646
Anthony Xiang <axiang@rams.colostate.edu>
2021-02-03 16:13:55 -0700

    added array for clicks and deleted triplist.js

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -6,7 +6,7 @@ import Header from "./Margins/Header";
 import Footer from "./Margins/Footer";
 import About from "./About/About";
 import Atlas from "./Atlas/Atlas";
-import TripList from "./Atlas/TripList";
+
 
 import {LOG} from "../utils/constants";
 import * as configSchema from "../../schemas/ConfigResponse";
@@ -41,6 +41,7 @@ export default class Page extends Component {
 				<Header toggleAbout={this.toggleAbout}/>
 				{this.renderAbout()}
 				{this.renderAtlas()}
+				
 				<Footer
 					serverSettings={this.state.serverSettings}
 					processServerConfigSuccess={this.processServerConfigSuccess}
@@ -61,7 +62,7 @@ export default class Page extends Component {
 		return (
 			<Collapse isOpen={!this.state.showAbout}>
 				<Atlas showMessage={this.props.showMessage}/>
-                <TripList/>
+               
 			</Collapse>
 		);
 	}
