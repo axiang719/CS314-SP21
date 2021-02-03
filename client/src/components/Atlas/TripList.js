4773b5f78c188ceaa6f1ce515139a67e1a7f740c
QuercusFelis <37717887+QuercusFelis@users.noreply.github.com>
2021-02-02 20:53:27 -0700

    add TripList.js

---
+++ client/src/components/Atlas/TripList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Col, Container, Row} from 'reactstrap';
+
+export default class TripList extends Component {
+
+    render() {
+        return (
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                        Test
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
