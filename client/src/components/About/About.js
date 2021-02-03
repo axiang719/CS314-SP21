1cb41baf2f1555147816c5d282a90c0155514c61
QuercusFelis <37717887+QuercusFelis@users.noreply.github.com>
2021-02-02 17:07:57 -0700

    add card structure

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,33 +1,56 @@
 import React, {Component} from 'react';
 
-import {Container, Row, Col, Button} from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, CardHeader,
+    CardFooter, Container, Row, Col, Button
+} from 'reactstrap';
 
 import {CLIENT_TEAM_NAME} from "../../utils/constants";
 
 export default class About extends Component {
-
     render() {
+    const people = [{name: "abc d",
+                     bio: "placeholder bio",
+                     imagePath: "../src/static/images/imageName.jpg"},
+                    {name: "abc d",
+                     bio: "placeholder bio",
+                     imagePath: "../src/static/images/imageName.jpg"},
+                    {name: "abc d",
+                     bio: "placeholder bio",
+                     imagePath: "../src/static/images/imageName.jpg"},
+                    {name: "abc d",
+                     bio: "placeholder bio",
+                     imagePath: "../src/static/images/imageName.jpg"},
+                    {name: "abc d",
+                     bio: "placeholder bio",
+                     imagePath: "../src/static/images/imageName.jpg"}];
+
         return (
             <Container>
                 <Row>
                     <Col>
                         <h2>{CLIENT_TEAM_NAME}</h2>
                     </Col>
-
                     <Col xs="auto">
                         <Button color="primary" onClick={this.props.closePage} xs={1}>
                             Close
                         </Button>
                     </Col>
                 </Row>
-
                 <Row>
-                    <img src = "https://media.sproutsocial.com/uploads/2020/08/Social-for-Gamers.svg"
-                         className = "Team Image"
-                         alt = "...">
-                    </img>
+                    {people.map((person) => (
+                    <Col>
+                    <Card>
+                        <CardHeader tag="h4">{person.name}</CardHeader>
+                        <CardBody>
+                            <img width="100%" src={person.imagePath} alt={person.name} />
+                            <CardText>{person.bio}</CardText>
+                        </CardBody>
+                    </Card>
+                    </Col>
+                    ))}
                 </Row>
-            
             </Container>
         );
     }
