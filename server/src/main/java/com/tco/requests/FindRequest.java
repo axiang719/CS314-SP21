3e44167e1e2b7526454cc5ffa036ba6e5b63d0ab
Ethan Seefried <eseefrie@tokyo.cs.colostate.edu>
2021-02-10 16:08:09 -0700

    added import maria

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -3,6 +3,7 @@ package com.tco.requests;
 import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.mariadb.jdbc; 
 
 
 public class FindRequest extends RequestHeader {
