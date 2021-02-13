30127959d92a0d84470677d0bd7ee82d834b9bc5
Chad Minning <chadmin@colostate.edu>
2021-02-13 12:03:56 -0700

    Move generate 'type' SQL to Query class

--- server/src/main/java/com/tco/requests/Query.java
+++ server/src/main/java/com/tco/requests/Query.java
@@ -4,37 +4,45 @@ public class Query {
     private String resultQuery;
     private String match;
     private Integer limit;
+    private String type;
 
     public Query(String match) {
         this.match = match;
     }
 
     public String getDataQuery() {
-        generateStartingSql();
+        generateStartDataSql();
+        generateTypeSql();
 
         if (limit != null) {
-            resultQuery +=  " Limit " + Integer.toString(limit);
+            resultQuery += " Limit " + Integer.toString(limit);
         }
 
         return resultQuery + ";";
     }
 
-    private void generateStartingSql() {
-        if (resultQuery == null) {
-            resultQuery = "";
-        }
+    private void generateStartDataSql() {
         resultQuery = "SELECT world.name, world.latitude, world.longitude, " 
                     + "world.id, world.altitude, world.municipality, "
                     + "world.type, world.iso_region, world.iso_country, "
-                    + "world.home_link FROM world"
-                    + resultQuery;
-    
+                    + "world.home_link FROM world";
+
         if (match != null && !match.equals("")) {
             resultQuery += " WHERE world.name LIKE '%" + match + "%'";
         }
     }
 
+    private void generateTypeSql() {
+        if (type != null && !type.equals("")) {
+            resultQuery += " AND world.type LIKE '%" + type + "%'";
+        }
+    }
+
     public void setLimit(Integer limit) {
         this.limit = limit;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
--- server/src/test/java/com/tco/requests/TestQuery.java
+++ server/src/test/java/com/tco/requests/TestQuery.java
@@ -9,16 +9,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestQuery {
 
+    private String testQuery;
+
+    @BeforeEach
+    public void createConfigurationForTestCases() {
+        testQuery = "SELECT world.name, world.latitude, world.longitude, "
+        + "world.id, world.altitude, world.municipality, "
+        + "world.type, world.iso_region, world.iso_country, world.home_link "
+        + "FROM world WHERE world.name LIKE '%Epps Airpark%'";
+    }
     
     @Test
     @DisplayName("Testing basic query")
     public void testBasicQuery() {
         Query sql = new Query("Epps Airpark");
-        String testQuery = sql.getDataQuery();
-        assertEquals("SELECT world.name, world.latitude, world.longitude, "
-                    + "world.id, world.altitude, world.municipality, "
-                    + "world.type, world.iso_region, world.iso_country, world.home_link "
-                    + "FROM world WHERE world.name LIKE '%Epps Airpark%';", testQuery);
+        String resultQuery = sql.getDataQuery();
+        assertEquals(testQuery + ";", resultQuery);
     }
 
     @Test
@@ -26,12 +32,16 @@ public class TestQuery {
     public void testLimitQuery() {
         Query sql = new Query("Epps Airpark");
         sql.setLimit(5);
-        String testQuery = sql.getDataQuery();
-        assertEquals("SELECT world.name, world.latitude, world.longitude, "
-                    + "world.id, world.altitude, world.municipality, "
-                    + "world.type, world.iso_region, world.iso_country, world.home_link "
-                    + "FROM world WHERE world.name LIKE '%Epps Airpark%' "
-                    + "Limit 5;", testQuery);
+        String resultQuery = sql.getDataQuery();
+        assertEquals(testQuery + " Limit 5;", resultQuery);
     }
 
+    @Test
+    @DisplayName("Testing query with type")
+    public void testTypeQuery() {
+        Query sql = new Query("Epps Airpark");
+        sql.setType("small_airport");
+        String resultQuery = sql.getDataQuery();
+        assertEquals(testQuery + " AND world.type LIKE '%small_airport%';", resultQuery);
+    }
 }
