3809f760ba97398e92048b8282048645a97afda5
Ethan Seefried <eseefrie@tokyo.cs.colostate.edu>
2021-02-17 15:31:14 -0700

    bug fix for the file coordinatesInput.js

---
+++ client/src/components/Atlas/CoordinatesInput.js
@@ -0,0 +1,8 @@
+const { Container } = Reactstrap;
+const { InputGroup, InputGroupAddon, InputGroupText, Input } = Reactstrap;
+
+import Coordinates from "https://cdn.skypack.dev/coordinate-parser@1.0.7";
+
+class CoordinatesInput extends React.Component {
+
+}
