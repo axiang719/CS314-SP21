7624a120389a109bc092beb76945d1f75c15f830
Ethan Seefried <eseefrie@tokyo.cs.colostate.edu>
2021-02-17 13:55:06 -0700

    created componentDidMount method

--- client/src/components/Atlas/Atlas.js
+++ client/src/components/Atlas/Atlas.js
@@ -34,6 +34,10 @@ export default class Atlas extends Component {
     
     }
 
+    componentDidMount(previousProps, previousState, snapShot){
+       
+    }
+
     render() {
         return (
             <div>
