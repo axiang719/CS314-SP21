95b08eabcf40980a8309abbd2cfc2b90ef1d444c
Michael Young <coyote38@rams.colostate.edu>
2021-02-23 16:47:50 -0700

    basic skeleton for PlacesList

--- client/src/components/Atlas/PlacesList.js
+++ client/src/components/Atlas/PlacesList.js
@@ -2,5 +2,13 @@ import React, {Component} from 'react';
 import { Col, Row, Button} from 'reactstrap';
 
 export default class PlacesList extends Component {
+    constructor(props) {
+        super(props);
+    }
 
+    render() {
+        return (
+            <p>TODO</p>
+        )
+    }
 }
