d3fbd9df8cb3922d43c9d987a3467aab9ed2ca05
Chad Minning <chadmin@colostate.edu>
2021-03-01 21:43:00 +0000

    Fix the 'where' part of query

--- server/src/main/java/com/tco/requests/FindRequest.java
+++ server/src/main/java/com/tco/requests/FindRequest.java
@@ -25,7 +25,7 @@ public class FindRequest extends RequestHeader {
   private void populatePlaces() {
       Query query = new Query(match);
       query.setType(type);
-      query.setWhere(new ArrayList(where));
+      query.setWhere(where);
       query.setLimit(limit);
       String dataQuery = query.getDataQuery();
       places = Database.queryDB(dataQuery);
@@ -34,7 +34,7 @@ public class FindRequest extends RequestHeader {
   private void populateFound() {
       Query query = new Query(match);
       query.setType(type);
-      query.setWhere(new ArrayList(where));
+      query.setWhere(where);
       String dataQuery = query.getCountQuery();
       ArrayList<HashMap<String, String>> count = Database.queryDB(dataQuery);
       found = Integer.parseInt(count.get(0).get("row_count"));
