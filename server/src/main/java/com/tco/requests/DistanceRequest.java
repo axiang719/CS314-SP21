c926de75e6cfd1da15a8df637281d0a7895226e3
Anthony Xiang <axiang@rams.colostate.edu>
2021-03-03 00:39:59 +0000

    added file to fix my mistake

---
+++ server/src/main/java/com/tco/requests/DistanceRequest.java
@@ -0,0 +1,29 @@
+package com.tco.requests;
+
+import com.tco.requests.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DistanceRequest extends RequestHeader {
+  private Float earthRadius;
+  private ArrayList<Float> distances;
+  private ArrayList<HashMap<String, String>> places;
+  private final transient Logger log = LoggerFactory.getLogger(DistanceRequest.class);
+
+  @Override
+  public void buildResponse() {
+      log.trace("buildResponse -> {}", this);
+      
+  }
+
+ 
+  
+  /* The following methods exist only for testing purposes and are not used
+  during normal execution, including the constructor. */
+
+  public DistanceRequest() {
+    this.requestType = "distance";
+  }
+}
