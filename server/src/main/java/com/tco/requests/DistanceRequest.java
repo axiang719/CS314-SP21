ba24cb27c0cd7f863fc20ef8b284138ea3287d47
Chad Minning <chadmin@colostate.edu>
2021-03-03 22:03:41 +0000

    Add distances array method

--- server/src/main/java/com/tco/requests/DistanceRequest.java
+++ server/src/main/java/com/tco/requests/DistanceRequest.java
@@ -8,22 +8,48 @@ import org.slf4j.LoggerFactory;
 
 public class DistanceRequest extends RequestHeader {
   private Float earthRadius;
-  private ArrayList<Float> distances;
+  private ArrayList<Integer> distances;
   private ArrayList<HashMap<String, String>> places;
   private final transient Logger log = LoggerFactory.getLogger(DistanceRequest.class);
 
   @Override
   public void buildResponse() {
-      log.trace("buildResponse -> {}", this);
-      
+    log.trace("buildResponse -> {}", this);
+    fillDistancesList();
   }
 
- 
+  private void fillDistancesList() {
+    this.distances = new ArrayList<Integer>();
+    int size = places.size();
+    float previousLatitude;
+    float previousLongitude;
+
+    for(int i=0; i <= size; i++) {
+      HashMap<String, String> place = places.get(i % size);
+      float latitude = Float.parseFloat(place.get("latitude"));
+      float longitude = Float.parseFloat(place.get("longitude"));
+
+      if (i != 0) {
+        int resultDistance = 0; //TODO Add call to distance function here. Replace 0 with results.
+        this.distances.add(resultDistance);
+      }
+
+      previousLatitude = latitude;
+      previousLongitude = longitude;
+    }
+  }
+
   
   /* The following methods exist only for testing purposes and are not used
   during normal execution, including the constructor. */
 
   public DistanceRequest() {
-    this.requestType = "distance";
+    this.requestType = "distances";
+  }
+
+  public ArrayList<Integer> testDistanceList(ArrayList<HashMap<String, String>> places) {
+    this.places = places;
+    fillDistancesList();
+    return this.distances;
   }
 }
--- server/src/test/java/com/tco/requests/TestDistanceRequest.java
+++ server/src/test/java/com/tco/requests/TestDistanceRequest.java
@@ -1,5 +1,8 @@
 package com.tco.requests;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,10 +11,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestDistanceRequest {
-   
+
+    private DistanceRequest distanceRequest;
+    private ArrayList<HashMap<String, String>> testPlaces;
+
+    @BeforeEach
+    public void createConfigurationForTestCases() {
+        distanceRequest = new DistanceRequest();
+        testPlaces = new ArrayList<HashMap<String, String>>();
+    }
+
+    
     @Test
-    @DisplayName("Placeholder")
-    public void placeholder() {
-        assertEquals(true, true);
+    @DisplayName("Request type is \"distances\"")
+    public void testType() {
+        String type = distanceRequest.getRequestType();
+        assertEquals("distances", type);
+    }
+
+
+    @Test
+    @DisplayName("Check that distances list is filled")
+    public void testDistanceList() {
+        int expectedDistance = 0;
+        HashMap<String, String> place = new HashMap<String, String>();
+        place.put("name", "testPlace");
+        place.put("latitude", "50");
+        place.put("longitude", "50");
+        testPlaces.add(place);
+        testPlaces.add(place);
+        ArrayList<Integer> distances = distanceRequest.testDistanceList(testPlaces);
+        assertEquals(0, distances.get(0));
+        assertEquals(2, distances.size());
     }
 }
