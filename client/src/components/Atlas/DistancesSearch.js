c1f902aa0f178fb7f6fda45be091ded1f788010f
QuercusFelis <37717887+QuercusFelis@users.noreply.github.com>
2021-03-09 16:44:28 -0700

    remove react from DistancesSearch.js

--- client/src/components/Atlas/DistancesSearch.js
+++ client/src/components/Atlas/DistancesSearch.js
@@ -1,70 +1,43 @@
-import React, { Component } from 'react';
-import { Button, InputGroup, Input, FormFeedback, Container, Row } from 'reactstrap';
-
 import {LOG} from "../../utils/constants";
 import * as distancesSchema from "../../../schemas/DistancesResponse";
 import { isJsonResponseValid, sendServerRequest, getOriginalServerPort } from "../../utils/restfulAPI";
 import { LatLngBounds } from 'leaflet';
 
-export default class DistancesSearch extends Component{
-   constructor(props){
-      super(props);
+export default class DistancesSearch {
+    var request = {
+        requestType: "distances",
+        places: [],
+        earthRadius: null
+    };
+    var distances = []
 
-      this.sendDistancesRequest = this.sendDistancesRequest.bind(this);
-      this.processDistancesResponse = this.processDistancesResponse.bind(this);
-      this.processDistancesSuccess = this.processDistancesSuccess.bind(this);
-
-      this.state = {
-         distancesRequest: {
-            requestType: "distances",
-            places: [],
-            earthRadius: null
-         },
-         distances: []
-      };
-   };
-
-   render() {
-      return (
-         <div/>
-      );
-   }
-
-   addDistancesArrayElements = (accumulator, currentValue) => accumulator + currentValue;
+    addDistancesArrayElements = (accumulator, currentValue) => accumulator + currentValue;
    
-   sumDistances() {
-      return this.state.distances.reduce(addDistancesArrayElements);
-   }
+    sumDistances() {
+        return this.distances.reduce(addDistancesArrayElements);
+    }
    
-   sendDistancesRequest(request) {
-		sendServerRequest(request)
-			.then(distancesResponse => {
-				if (distancesResponse) {
-					this.processDistancesResponse(distancesResponse);
+    sendDistancesRequest(request) {
+	    sendServerRequest(request)
+		    .then(distancesResponse => {
+			    if (distancesResponse) {
+				    this.processDistancesResponse(distancesResponse);
 				} else {
-					this.props.showMessage("Distances Request To The Server Failed.", "error");
+					LOG.error("Distances Request To The Server Failed.");
 				}
 		    });
 	}
 
 	processDistancesResponse(distancesResponse) {
 		if (!isJsonResponseValid(distancesResponse, distancesSchema)) {
-			this.processDistancesRequestError("Distances Response Not Valid. Check The Server.");
+			LOG.error("Distances Response Not Valid. Check The Server.");
 		} else {
 			this.processDistancesSuccess(distancesResponse);
 		}
 	}
 
    processDistancesSuccess(distancesResponse) {
-		LOG.info("Receiving distances response from:", getOriginalServerPort());
-      const distances = distancesResponse.distances;
-		this.setState({ 'distances' : distances });
+       LOG.info("Receiving distances response from:", getOriginalServerPort());
+       distances = distancesResponse.distances;
 	}
-
-	processDistancesRequestError(message) {
-		LOG.error(message);
-		this.props.showMessage(message, "error");
-	}
-   
-
 } 
