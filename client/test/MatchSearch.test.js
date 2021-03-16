01aa02529e7fe05041c86542749c6fe02324e4b1
Anthony Xiang <axiang@rams.colostate.edu>
2021-03-16 03:55:09 +0000

    added more test cases

--- client/test/MatchSearch.test.js
+++ client/test/MatchSearch.test.js
@@ -9,35 +9,133 @@ import { beforeEach, expect, it } from '@jest/globals';
 describe('MatchSearch', () => {
 
   let matchSearchWrapper;
-  const createSnackBar = jest.fn();
+  const snacks= jest.fn();
+  const request = { requestType: 'find',
+                    match : "dave", 
+                    type: ["airport"], 
+                    where: ["United States"],
+                    limit: 1
+                };
+    const badRequest ={
+        requestType: 'find',
+        match : "pickle", 
+        
+        limit: 5
+
+    };
+    const emptyRequest={
+              requestType: 'find',
+              mathc: null
+    };
 
     beforeEach(() => {
-      matchSearchWrapper = shallow(<MatchSearch match = {createSnackBar} />);
+        mockFindResponse();
+        matchSearchWrapper = shallow(<MatchSearch 
+                                                setType={["airport"]}
+                                                setWhere={["United States"]}
+                                                renderDropdown={snacks} 
+                                                setMarker={snacks}
+                                                showMessage={snacks}
+                                                focus={"match"}
+                                                match={"dave"}
+                                               
+                                                
+                                                />);
     });
     
         it('initializes as expected', () => {
-            simulateOnClickEvent(matchSearchWrapper);
-                setTimeout( () => {
-                    
-                    const actualSearchType = matchSearchWrapper.state().focus;
-                    const expectedSearchType = "match";
-                    expect(actualSearchType).toEqual(expectedSearchType);}, 10);
-                });
+            const actualSearchType = matchSearchWrapper.state().focus;
+            const expectedSearchType = "match";
+            expect(actualSearchType).toEqual(expectedSearchType);
+        });
+
+        it('toggles the Modal', () => {
+            matchSearchWrapper.instance().toggleModal();
+            const expectedToggle = true;
+            const actualToggle = matchSearchWrapper.state().modalOpen;
+            expect(actualToggle).toEqual(expectedToggle);
+        });
 
         it('updates input as expected', () => {
-            simulateInput(matchSearchWrapper, "pickle");
-            expect(matchSearchWrapper.state().findRequest.match).toEqual("pickle");
+            simulateInput(matchSearchWrapper, "dave");
+            expect(matchSearchWrapper.state().findRequest.match).toEqual("dave");
+         
         });
+
+        it('set Where as expected', () => {
+            matchSearchWrapper.instance().setWhere();
+            const expectedWhere = "United States";
+            setTimeout( () => {
+                const acutalWhere =  matchSearchWrapper.state().findRequest.where;
+                expect(acutalWhere).toEqual(expectedWhere);}, 10);
     
+        });
+
+        it('set Type as expected', () => {
+            matchSearchWrapper.instance().setType();
+            const expectedType = "airport";
+            setTimeout( () => {
+                const acutalWhere =  matchSearchWrapper.state().findRequest.type;
+                expect(acutalWhere).toEqual(expectedWhere);}, 10);
+    
+        });
+
+    
+        
+      
+    
+        it('Sends request to api', () => {
+           matchSearchWrapper.instance().sendFindRequest(request);
+           matchSearchWrapper.instance().sendFindRequest(badRequest);
+           simulateInput(matchSearchWrapper,"bad Input");
+           expect(matchSearchWrapper.state().findRequest.match).toEqual(null);
+
+            setTimeout( () => {
+                const actualFocus= matchSearchWrapper.state().focus;
+                expect(actualFocus).toEqual("match");}, 10);
+    
+        });
+
         function simulateInput(wrapper, input) {
             wrapper.find('Input').at(0).simulate('change', { target: { value: input } });
             wrapper.update();
         };
         
-        function simulateOnClickEvent(wrapper) {
-            wrapper.find('button').at(0).simulate('click');
-            wrapper.update();
-        };
+        function mockFindResponse() {
+            const responseData = {
+                
+                    match: "dave",
+                    limit: 1,
+                    found: 18,
+                    places: [
+                        {
+                            "continent": "North America",
+                            "altitude": "5170",
+                            "country": "United States",
+                            "latitude": "40.0332984924",
+                            "name": "Dave's Airport",
+                            "municipality": "Louisville",
+                            "type": "small_airport",
+                            "region": "Colorado",
+                            "longitude": "-105.124000549"
+                        }
+                    ],
+                    type: [
+                        "airport"
+                    ],
+                    where: [
+                        "United States"
+                    ],
+                    requestType: "find"
+                
+            };
+            fetch.mockResponse(JSON.stringify(responseData));
+        }
+        
+        // function simulateOnClickEvent(wrapper) {
+        //     wrapper.find('button').at(0).simulate('click');
+        //     wrapper.update();
+        // }; 
 
   
 
