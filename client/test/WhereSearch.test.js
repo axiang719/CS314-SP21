fb1efc3f4a31f46654d4b57913ea27babf08537b
Ethan Seefried <eseefrie@colostate.edu>
2021-03-17 17:37:58 +0000

    added test case to Wheresearch.test

--- client/test/WhereSearch.test.js
+++ client/test/WhereSearch.test.js
@@ -8,18 +8,34 @@ import WhereSearch from '../src/components/Atlas/WhereSearch';
 
 describe('WhereSearch', () => {
     let whereSearchWrapper;
+    const helper = jest.fn();
+    const whereArray = [""];
 
     beforeEach(() => {
-        whereSearchWrapper = shallow(<WhereSearch/>);
+        whereSearchWrapper = shallow(<WhereSearch where = {whereArray}
+                                                  whereType = {helper}
+                                                  processFocus = {helper}
+                                                  setWhere = {helper}/>);
         whereSearchWrapper.state().show = true;
+        whereSearchWrapper.state().whereValue = "tokyo";
         whereSearchWrapper.update();
     });
 
     it('initializes as expected', () => {
         const actualWhereValue = whereSearchWrapper.state().whereValue;
-        const expectedWhereValue = "";
+        const expectedWhereValue = "tokyo";
 
         expect(whereSearchWrapper.state().show).toEqual(true);
         expect(actualWhereValue).toEqual(expectedWhereValue);
     });
+    it('tests processOnClickWhere function',() => {
+        const actualWhereValue = whereSearchWrapper.state().whereValue;
+        const expectedWhereValue = "tokyo";
+        
+        whereSearchWrapper.instance().processOnClickWhere();
+        expect(actualWhereValue).toEqual(expectedWhereValue);
+    });
+    
+
+
 });
