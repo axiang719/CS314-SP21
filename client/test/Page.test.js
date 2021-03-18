158fdf366269217c040eec67fd102a85e16060da
Ethan Seefried <eseefrie@colostate.edu>
2021-03-18 04:10:33 +0000

    added shell testing

--- client/test/Page.test.js
+++ client/test/Page.test.js
@@ -9,14 +9,21 @@ import { beforeEach, expect, it } from '@jest/globals';
 describe('Page', () => {
     
     let pageWrapper;
+    const helper = jest.fn();
 
     beforeEach(() => {
-        pageWrapper = shallow(<Page/>);
+        pageWrapper = shallow(<Page
+                            showMessage = {helper}/>);
     });
 
     it('toggles as expected', () => {
         pageWrapper.setState({showAbout: true});
         pageWrapper.instance().toggleAbout();
         expect(pageWrapper.state().showAbout).toEqual(false);
+    });
+
+    it('tests process config response',()=>{
+        pageWrapper.instance().processServerConfigError("this is a test error");
+
     });
 });
