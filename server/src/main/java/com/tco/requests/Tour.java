0d9de9211fb287177f96d442f1fc04cedac46876
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-03-26 12:27:54 +0000

    add sorting algorithm

--- server/src/main/java/com/tco/requests/Tour.java
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -11,6 +11,14 @@ public class Tour {
 	private boolean tourDistanceIsDirty;
 	private ArrayList<HashMap<String, String>> places;
     
+	public Tour(Tour tour) {
+		this(tour.getEarthRadius(), tour.getPlaces().clone());
+	}
+
+	public Tour(int earthRadius, HashMap<String, String> place) {
+		this(earthRadius, new ArrayList<>(place));
+	}
+
 	public Tour(int earthRadius, ArrayList<HashMap<String, String>> places) {
 		this.earthRadius = earthRadius;
 		this.places = places;
@@ -18,7 +26,47 @@ public class Tour {
 		tourDistanceIsDirty = true;
 	}
 
-	public void appendPlaces(HashMap<String, String> place) {
+	static public Tour sortPlacesByDistance(Tour tour, int startingIndex, int lookAheadLimit) {
+		//base case
+		if(tour.size() <= 2) { 
+			return tour;
+		}
+		//recursion case
+		Tour tempTour = new Tour(tour);
+		HashMap<String, String> start = tempTour.remove(startingIndex);
+		double startLatitude = Double.parseDouble(start.get(0));
+		double startLongitude = Double.parseDouble(start.get(1));
+		if(lookAheadLimit == 0 || lookAheadLimit > tour.size()) {
+			lookAheadLimit = tour.size();
+		}
+		int shortestDistance = Integer.MAX_VALUE;
+		int closestNeighborIndex = 0;
+		int neighborIndexDistance = 0;
+		int i = startingIndex;
+		while(neighborIndexDistance < lookAheadLimit) {
+			if(i == tour.size()){
+				i = 0;
+			}
+			HashMap<String, String> neighbor = tempTour.getPlaces().at(i);
+			double neighborLatitude = Double.parseDouble(neighbor.get(0));
+			double neighborLongitude = Double.parseDouble(neighor.get(1));
+
+			DistancesRequest dr = new DistancesRequest();
+			dr.setRadius = tour.getEarthRadius();
+			int distance = dr.calculateDistance(startLatitude, startLongitude, neighborLatitude, neighborLongitude);
+			if(distance < shortestDistance) {
+				shortestDistance = distance;
+				closestNeighborIndex = i;
+			}
+			i++;
+			neighborIndexDistance++;
+		}
+		shortTour = new Tour(tour.getEarthRadius(), start);
+		shortTour.appendPlace(tempTour.removePlace(closestNeighborIndex));
+		return shortTour.appendTour(sortPlacesByDistance(tempTour, i, lookAheadLimit));
+	}
+
+	public void appendPlace(HashMap<String, String> place) {
 		places.add(place);
 		tourDistanceIsDirty = true;
 	}
@@ -31,6 +79,10 @@ public class Tour {
 
 	public ArrayList<HashMap<String, String>> getPlaces() {
 		return places;
+	}
+
+	public int size() {
+		return places.size();
 	}
 
 	public Integer getTourDistance() {
