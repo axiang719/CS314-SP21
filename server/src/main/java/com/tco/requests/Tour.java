7005bac961c85e1376b9be92781926cf332bf2d0
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-03-26 10:12:05 +0000

    add total distance calculation to Tour

--- server/src/main/java/com/tco/requests/Tour.java
+++ server/src/main/java/com/tco/requests/Tour.java
@@ -5,18 +5,38 @@ import java.util.ArrayList;
 import java.lang.Math;
 
 public class Tour {
-	private int TourDistance;
+	private int tourDistance;
+	private boolean tourDistanceIsDirty;
 	private ArrayList<HashMap<String, String>> places;
     
 	public Tour(ArrayList<HashMap<String, String>> places) {
 		this.places = places;
+		tourDistance = 0;
+		tourDistanceIsDirty = true;
 	}
 
 	public ArrayList<HashMap<String, String>> getPlaces() {
 		return places;
 	}
 
-	public int getTourDistance() {
+	public Integer getTourDistance() {
+		if(TourDistanceIsDirty) {
+			findTourDistance()
+		}
 		return TourDistance;
+	}
+
+	private void findTourDistance() {
+		int tourDistance = 0;
+
+		DistanceRequest dr = new DistanceRequest;
+		dr.setRadius(6371);
+		ArrayList<Integer> distances = dr.testDistancesList(places);
+		for(Integer distance:distances) {
+			tourDistance += distance.intValue();
+		}
+
+		this.tourDistance = tourDistance;
+		tourDistanceIsDirty = false;
 	}
 }
