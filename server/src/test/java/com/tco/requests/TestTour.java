cb1184072d2a127f679aba28916e5d55d16596d4
Ethan Seefried <eseefrie@colostate.edu>
2021-03-30 18:52:04 +0000

    added before each for testTour

--- server/src/test/java/com/tco/requests/TestTour.java
+++ server/src/test/java/com/tco/requests/TestTour.java
@@ -1,5 +1,17 @@
-public class TestTourRequest {
-    private TourRequest tourRequest;
- 
+package com.tco.requests;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTour {
+    private Tour tour;
+
+    @BeforeEach
+    public void createConfigurationForTestCases() {
+        tour = new Tour();
+    }
 }
