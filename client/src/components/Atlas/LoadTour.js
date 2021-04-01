ddad2c23d28381bc8f9f06703f0ed104982b09ec
Chad Minning <chadmin@colostate.edu>
2021-04-01 23:07:25 +0000

    Add LoadTour Modal

--- client/src/components/Atlas/LoadTour.js
+++ client/src/components/Atlas/LoadTour.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, Input } from 'reactstrap';
 
 export default class LoadTour extends Component {
     constructor(props) {
 		super(props);
+
+        this.renderModal = this.renderModal.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+
+        this.state = {
+            modalOpen: false
+        }
     }
 
     render() {
-        return (
-            <Button color="primary">Load</Button>
+        return ( 
+            <>
+                <Button color="primary" onClick={this.toggleModal}>Load</Button>
+                {this.renderModal()}
+            </>
         );
     }
+
+    renderModal() {
+        return (
+            <Modal isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>
+                    <div className="text-center">Load Tour</div>
+                </ModalHeader>
+                <ModalBody>
+                    {this.renderInput()}                    
+                </ModalBody>
+            </Modal>
+        );
+    }
+
+    toggleModal() {
+        const { modalOpen } = this.state;
+        this.setState({ modalOpen: !modalOpen });
+    }
+
+    renderInput() {
+        return (
+            <>
+            </>
+        )
+    }
+
 }
