bf810c80ae102a2c6fa055bc8c14fbf5ab392a39
Ethan Seefried <eseefrie@colostate.edu>
2021-04-06 03:18:21 +0000

    added uploadJsonFile function

--- client/src/components/Atlas/LoadTour.js
+++ client/src/components/Atlas/LoadTour.js
@@ -120,6 +120,19 @@ export default class LoadTour extends Component {
         }
     }
 
+    uploadJsonFile(e){
+        let jsonRows =[];
+                const files = e.target.files, file = files[0];
+                let reader = new FileReader();
+
+                reader.onload = function(e) {
+                    let data = JSON.parse(e.target.result);
+                    console.log(data);
+                };
+                reader.readAsText(file);
+        }
+    }
+
 
     upload(e){
         let jsonRows = [];
