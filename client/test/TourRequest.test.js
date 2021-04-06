570e893da952702240de01c5f45bdc4e5bfa6f64
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-04-06 02:29:48 +0000

    tailor tests for actual desired functionality

--- client/test/TourRequest.test.js
+++ client/test/TourRequest.test.js
@@ -1,32 +1,33 @@
 import './jestConfig/enzyme.config.js';
 import {shallow} from 'enzyme';
-
 import { beforeEach, expect, it } from '@jest/globals';
 import TourRequest from '../src/components/Atlas/TourRequest.js';
 
 describe('Tour Request', () => {
-
-    const showMessage = jest.fn();
-    const request = { requestType: 'tour',
-            earthRadius: 3959.0,
-	    response: 1.0,
-            places: []
+    let tourRequest;
+    const request = { 
+	requestType: 'tour',
+        earthRadius: 3959.0,
+	response: 1.0,
+        places: []
     };
 
     beforeEach(() => {
-        mockTourResponse();
+	tourRequest = TourRequest(request.places, request.earthRadius);
     });
 
     it('Sends request to api', () => {
-	    const response = TourRequest.sendRequest(request);
-	    expect(response.requestType).toEqual(request.requestType);
-	    expect(response.earthRadius).toEqual(request.earthRadius);
-	    expect(response.response).toEqual(request.response);
-	    expect(response.places).toEqual(request.places);
+        mockTourResponse();
+	const response = tourRequest.sendRequest();
+	    
+	expect(response.requestType).toEqual(request.requestType);
+	expect(response.earthRadius).toEqual(request.earthRadius);
+	expect(response.response).toEqual(request.response);
+	expect(response.places).toEqual(request.places);
     });
 
-    it('Sends fails with bad api request', () => {
-        const response = TourRequest.sendRequest({"name": "and nothing else"});
+    it('Appropriately handles missing response', () => {
+        const response = tourRequest.sendRequest();
         expect(response).toEqual(null);
     });
 
