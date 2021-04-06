ba6f004575f0c64d8042d2acef2997fcf41190c4
Ethan Seefried <eseefrie@colostate.edu>
2021-04-06 21:40:57 +0000

    added imports and class heading

---
+++ client/src/components/Atlas/SaveTour.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import XLSX from "xlsx";
+
+import { Button, Modal, ModalHeader, ModalBody, Input, Form, FormGroup, FormText, Row, Col } from 'reactstrap';
+
+
+export default class SaveTour extends Component {
+
+
+}
