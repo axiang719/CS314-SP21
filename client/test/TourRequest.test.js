d7d88ccaab43372e3c9e0ffcc9a11a0d6ee90815
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-04-06 01:50:06 +0000

    remove React testing sections

--- client/test/TourRequest.test.js
+++ client/test/TourRequest.test.js
@@ -6,8 +6,6 @@ import TourRequest from '../src/components/Atlas/TourRequest.js';
 
 describe('Tour Request', () => {
 
-    let tourRequestWrapper;
-    
     const showMessage = jest.fn();
     const request = { requestType: 'tour',
             earthRadius: 3959.0,
@@ -17,30 +15,15 @@ describe('Tour Request', () => {
 
 
     beforeEach(() => {
-        mockDistancesResponse();
-        distancesSearchWrapper = shallow(<DistancesSearch
-                                            showMessage = {showMessage}
-                                        />);
-        console.log(distancesSearchWrapper);
+        mockTourResponse();
     });
-
-
-    it('initializes as expected', () => {
-        const actualRequestType = distancesSearchWrapper.state().distancesRequest.requestType;
-        const expectedRequestType = "distances";
-
-        expect(actualRequestType).toEqual(expectedRequestType);
-    });
-
 
     it('Sends request to api', () => {
-        distancesSearchWrapper.instance().sendDistancesRequest(request);
         setTimeout( () => {
             const actualDistances = distancesSearchWrapper.state().distances;
             expect(actualDistances).toEqual([500]);}, 10);
 
     });
-
 
     it('Sends fails with bad api request', () => {
         distancesSearchWrapper.instance().sendDistancesRequest({"name": "and nothing else"});
@@ -57,6 +40,4 @@ describe('Tour Request', () => {
         };
         fetch.mockResponse(JSON.stringify(responseData));
     }
-
-
 });
