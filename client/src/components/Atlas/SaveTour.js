b3fec9e3753b6becac639660843c1dfb9f766dfa
Ethan Seefried <eseefrie@colostate.edu>
2021-04-07 16:44:32 +0000

    added downloadFile to restfulAPI

--- client/src/components/Atlas/SaveTour.js
+++ client/src/components/Atlas/SaveTour.js
@@ -3,6 +3,7 @@ import XLSX from "xlsx";
 
 import { Button, Modal, ModalHeader, ModalBody, Input, Form, FormGroup, FormText, Row, Col } from 'reactstrap';
 import { test } from '@jest/globals';
+import { downloadFile } from '../../utils/restfulAPI'
 
 
 export default class SaveTour extends Component {
@@ -10,5 +11,7 @@ export default class SaveTour extends Component {
     constructor(props){
         super(props);
     }
-    
-}
+
+    //JSON.stringify
+    //downloadFile();
+}  
