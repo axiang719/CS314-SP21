bbc106927574146907398701d0afd46016a099d3
Anthony Xiang <axiang@rams.colostate.edu>
2021-04-08 00:32:55 +0000

    added method for converting floats to strings

--- client/src/components/Atlas/SaveTour.js
+++ client/src/components/Atlas/SaveTour.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import json2csv from 'json2csv';
 import { Button, Modal, ModalHeader, ModalBody, Input, Form, FormGroup, FormText, Row, Col } from 'reactstrap';
 
 
@@ -8,7 +8,9 @@ export default class SaveTour extends Component {
 
     constructor(props){
         super(props);
-    }
+        this. convertListOfClicksToString = this.convertListOfClicksToString.bind(this);
+       
+    }   
 
     render() {
         return ( 
@@ -17,5 +19,17 @@ export default class SaveTour extends Component {
             </>
         );
     }
-    
-}
+
+    convertListOfClicksToString(){
+        const listOfClicks = this.props.listOfClicks;
+        let data = [];
+        for(let i = 0; i < listOfClicks.length; i++){
+            const place = {
+                latitude: listOfClicks[i].latitude.toString(),
+                longitude: listOfClicks[i].longitude.toString()
+            }
+            data.push(place);
+        }
+        return data; 
+    }
+ }
