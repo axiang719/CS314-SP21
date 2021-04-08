28715453a37c8ffdf3a17d40e09dce36627df0d5
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-04-08 19:30:16 +0000

    add save modal

--- client/src/components/Atlas/SaveTour.js
+++ client/src/components/Atlas/SaveTour.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import XLSX from "xlsx";
 import { Button, Modal, ModalHeader, ModalBody, Input, Form, FormGroup, FormText, Row, Col } from 'reactstrap';
 
-
-
 export default class SaveTour extends Component {
     constructor(props){
         super(props);
@@ -11,14 +9,34 @@ export default class SaveTour extends Component {
         this.exportCSV = this.exportCSV.bind(this);
       
     }   
-
+        
     render() {
-        return ( 
+        return (
             <>
-                <Button id="Save" color="primary" onClick = {this.convertListOfClicksToString} >Save</Button>  
+                <Button id="Save" color="primary" onClick={this.toggleModal}>Save</Button>
+                {this.renderModal()}
             </>
         );
     }
+
+    renderModal() {
+        return (
+            <Modal isOpen={this.state.modalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>
+                    <div className="text-center">Save Tour</div>
+                </ModalHeader>
+                <ModalBody>
+                	<Button id="SaveCSV" color="primary" onClick={this.convertListOfClicksToString}>Save as CSV</Button>  
+                </ModalBody>
+            </Modal>
+        );
+    }
+
+    toggleModal() {
+        const { modalOpen } = this.state;
+        this.setState({ modalOpen: !modalOpen, validTour: null });
+    }
+
 
     convertListOfClicksToString(){
         let data = [];
@@ -26,17 +44,17 @@ export default class SaveTour extends Component {
         this.exportCSV(data);
     }
 
-        exportCSV(csvExport){
-            const wb = XLSX.utils.book_new();
-            const ws = XLSX.utils.json_to_sheet(csvExport)
-            XLSX.utils.book_append_sheet(wb, ws, "info")
-            const wopts = {
-                bookType: 'csv',
-                bookSST: false,
-                type: 'buffer'
-            };
-            const wbout = XLSX.writeFile(wb, "output.csv" ,wopts);
-        }
+    exportCSV(csvExport){
+        const wb = XLSX.utils.book_new();
+        const ws = XLSX.utils.json_to_sheet(csvExport)
+        XLSX.utils.book_append_sheet(wb, ws, "info")
+        const wopts = {
+            bookType: 'csv',
+            bookSST: false,
+            type: 'buffer'
+        };
+        const wbout = XLSX.writeFile(wb, "output.csv" ,wopts);
+    }
 }
 
  
