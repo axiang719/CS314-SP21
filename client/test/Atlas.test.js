a4fb2f61d63b3a56869573955b5478a07d4dd2ef
Ethan Seefried <eseefrie@colostate.edu>
2021-05-04 04:42:51 +0000

    added testing

--- client/test/Atlas.test.js
+++ client/test/Atlas.test.js
@@ -160,6 +160,11 @@ describe('Atlas', () => {
         let value = atlasWrapper.instance().getPolyStyle();
         expect('10,20').toEqual(value);
     });
+
+    it('tests turn lines off', ()=>{
+        atlasWrapper.setState({linesOn: true});
+        atlasWrapper.instance().turnLinesOff();
+    });
     
     function mockGeoLocateResponse() {
         const geoResponseData = {
