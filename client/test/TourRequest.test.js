e16169c817c7304c94b047af965c175e2d8db232
quercusfelis <37717887+QuercusFelis@users.noreply.github.com>
2021-05-06 21:28:03 +0000

    add server port

--- client/test/TourRequest.test.js
+++ client/test/TourRequest.test.js
@@ -20,7 +20,7 @@ describe('Tour Request', () => {
     it('Sends request to api', () => {
         mockTourResponse();
 	setTimeout( () => {
-	        const response = tourRequest.sendRequest(31413);  
+	        const response = tourRequest.sendRequest("31413");  
 	        expect(response.requestType).toEqual(request.requestType);
 	        expect(response.earthRadius).toEqual(request.earthRadius);
 	        expect(response.response).toEqual(request.response);
@@ -28,9 +28,19 @@ describe('Tour Request', () => {
     	}, 10);
     });
 
+    async handleShortTourClick() {
+        const length = this.props.listOfClicks.length
+        if (length >= 2) {
+            const oldList = this.props.getPlaces();
+            const i = new TourRequest(oldList,3539);
+            await i.sendRequest(this.props.serverSettings.serverPort);
+            const newList = i.getPlaces();
+            this.props.setTour(newList);
+    }
+
     it('Appropriately handles missing response', () => {
 	setTimeout( () => {
-	    const response = tourRequest.sendRequest();
+	    const response = tourRequest.sendRequest("31413");
             expect(response).toEqual({});
     	}, 10);
     });
